37426bf3c24feb12616590e0a15573578d990a7f
Paulo Erak <pauloerak2001@gmail.com>
2025-06-21 17:30:49 +0200

    Remove class Rewards

--- test_codes/csharp/csharp/HS-Sync/SyncHSPlanner.cs
+++ test_codes/csharp/csharp/HS-Sync/SyncHSPlanner.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using RewardsClass;
 using Trees;
 using System.Diagnostics;
 using System.IO;
--- test_codes/csharp/csharp/Program.cs
+++ test_codes/csharp/csharp/Program.cs
@@ -2,7 +2,6 @@
 using NetMQ.Sockets;
 using System;
 using System.Text;
-using RewardsClass;
 using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel.Design;
